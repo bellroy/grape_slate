ed1cc8262deeaabe1b96c337a53ba33f808282a7
psiedlak <phouston88@gmail.com>
2015-10-27 17:15:41 +1100

    fix specs

--- spec/grape_slate/generators_spec.rb
+++ spec/grape_slate/generators_spec.rb
@@ -5,10 +5,11 @@ describe GrapeSlate::Generators::Base do
 
   describe '#documentable_route_path' do
     let(:route) { Grape::Route.new }
+    let(:base_path) { 'http://example.org' }
     subject(:documentable_route_path) { generator.documentable_route_path(route)}
 
     before do      
-      allow(GrapeSlate).to receive_message_chain(:configuration, :base_path).and_return('http://example.org')
+      allow(GrapeSlate).to receive_message_chain(:configuration, :base_path).and_return(base_path)
     end
 
     #TODO : improve stubbing so there's no need for route_path/route_version mocks ( should come from resource object)
@@ -18,7 +19,7 @@ describe GrapeSlate::Generators::Base do
         allow_any_instance_of(Grape::Route).to receive(:route_version).and_return("v1")
       end
 
-      it { is_expected.to eq "http://abc.com/v1/cases/:id/studies" }
+      it { is_expected.to eq "#{base_path}/v1/cases/:id/studies" }
     end
 
     context "when api version not present" do
@@ -27,7 +28,7 @@ describe GrapeSlate::Generators::Base do
         allow_any_instance_of(Grape::Route).to receive(:route_version).and_return(nil)
       end
 
-      it { is_expected.to eq "http://abc.com/cases/:id/studies" }
+      it { is_expected.to eq "#{base_path}/cases/:id/studies" }
     end    
   end
 end
